5a003cc485bd78fffa46e786adcbb8f24fd93561
Irtaza Ahmed <irtazaahmedk@gmail.com>
2025-01-20 20:46:53 +0500

    serving html and css file in node js

---
+++ app.js
@@ -0,0 +1,39 @@
+import { readFile } from 'fs/promises'
+import { createServer } from 'http'
+import path from 'path'
+
+const PORT = 3000
+
+const serveFile = async (res, filePath, contentType)=> {
+    try {
+                
+        const data = await readFile(filePath)
+        res.writeHead(200, { "Content-Type": contentType })
+        res.end(data)
+    } catch (error) {
+        res.writeHead(404, { "Content-Type": "text/html" })
+        res.end('404 Page not Found')
+    }
+}
+
+const server = createServer(async (req, res) => {
+    console.log(req.url);
+
+    if (req.method === 'GET') {
+        if (req.url === '/') {
+            return serveFile(res, path.join("public", "index.html"), 'text/html')
+        } else if (req.method === 'GET') {
+            if (req.url === '/style.css') {
+
+                return serveFile( res, path.join("public", "style.css"), 'text/css' )
+                
+            }
+        }
+    }
+
+})
+
+server.listen(PORT, () => {
+    console.log(`server running at localhost:3000`);
+
+})
