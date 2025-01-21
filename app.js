dcbcc1a2f4e02b311d11769a53b1a43d2c723c18
Irtaza Ahmed <irtazaahmedk@gmail.com>
2025-01-21 16:34:00 +0500

    handling form submission

--- app.js
+++ app.js
@@ -1,19 +1,45 @@
-import { readFile } from 'fs/promises'
+import { readFile, writeFile } from 'fs/promises'
 import { createServer } from 'http'
+import crypto from 'crypto'
 import path from 'path'
 
 const PORT = 3000
+const DATA_FILE = path.join("data", "links.json")
 
-const serveFile = async (res, filePath, contentType)=> {
+const serveFile = async (res, filePath, contentType) => {
     try {
-                
+        // file ka data variable mein save hojaayega
         const data = await readFile(filePath)
+
+        // file ka meta data server ko pata chale isliye
         res.writeHead(200, { "Content-Type": contentType })
+
+        // file client ko serve karega
         res.end(data)
+
     } catch (error) {
-        res.writeHead(404, { "Content-Type": "text/html" })
+        res.writeHead(404, { "Content-Type": "text/plain" })
         res.end('404 Page not Found')
     }
+}
+
+const loadLinks = async () => {
+    try {
+        const data = await readFile(DATA_FILE, 'utf-8')
+        return JSON.parse(data)
+    } catch (error) {
+        // error no entry
+        if (error.code === "ENOENT") {
+            await writeFile(DATA_FILE, JSON.stringify({}))
+            return {}
+        }
+        throw error
+    }
+}
+
+const saveLinks = async (links) => {
+    await writeFile(DATA_FILE, JSON.stringify(links))
+
 }
 
 const server = createServer(async (req, res) => {
@@ -22,13 +48,45 @@ const server = createServer(async (req, res) => {
     if (req.method === 'GET') {
         if (req.url === '/') {
             return serveFile(res, path.join("public", "index.html"), 'text/html')
-        } else if (req.method === 'GET') {
-            if (req.url === '/style.css') {
+        } else if (req.url === '/style.css') {
 
-                return serveFile( res, path.join("public", "style.css"), 'text/css' )
-                
-            }
+            return serveFile(res, path.join("public", "style.css"), 'text/css')
+
         }
+
+    }
+
+    if (req.method === 'POST' && req.url === '/shorten') {
+        const links = await loadLinks()
+        let body = ''
+        req.on('data', (chunk) => body += chunk)
+        req.on('end', async () => {
+            console.log(body);
+            const { url, shortCode } = JSON.parse(body)
+
+            if (!url) {
+                res.writeHead(400, { "Content-Type": "text/plain" })
+                return res.end("URL is required")
+            }
+
+            console.log(shortCode);
+            console.log(links);
+            
+            
+            const finalShortCode = shortCode || crypto.randomBytes(4).toString('hex')
+            console.log(finalShortCode);
+            
+            if (links[finalShortCode]) {
+                res.writeHead(400, { "Content-Type": "text/plain" })
+                return res.end("Short code is already exist. Please choose another.")
+            }
+
+            links[finalShortCode] = url
+            await saveLinks(links)
+            res.writeHead(200, { "Content-Type": "application/json" })
+            res.end(JSON.stringify({ success: true, shortCode: finalShortCode }))
+
+        })
     }
 
 })
